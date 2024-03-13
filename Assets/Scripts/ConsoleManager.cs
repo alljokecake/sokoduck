403facbcfbd572c770ae6b00600c0cc5f7dbc437
alljokecake <ilhanburcakbas@gmail.com>
2024-03-14 01:09:45 +0300

    refactor commands

--- Assets/Scripts/ConsoleManager.cs
+++ Assets/Scripts/ConsoleManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,8 @@ using TMPro;
 
 public class ConsoleManager : MonoBehaviour
 {
-    public int maxHistory = 25;
+    // @FIXME: history stuff
+    // (public int maxHistory = 100;
 
     public GameObject consolePanel, textObject;
     public TMP_InputField consoleBox;
@@ -15,66 +17,86 @@ public class ConsoleManager : MonoBehaviour
     [SerializeField]
     List<Command> commandHistory = new List<Command>();
 
+// ----------------------------------------------------------------------------
+//     - Manage Commands -
+// ----------------------------------------------------------------------------
+
+    private Dictionary<string, Action<string[]>> commands;
+
+    public ConsoleManager () {
+        commands = new Dictionary<string, Action<string[]>>();
+
+        RegisterCommand("level", LevelCommand);
+        RegisterCommand("clear", ClearCommand);
+        RegisterCommand("help", HelpCommand);
+    }
+
+    public void RunCommand(string input) {
+        string[] parts = input.Split(' ');
+        string command = parts[0];
+        string[] arguments = parts.Length > 1 ? parts[1..] : new string[0];
+        Array.Copy(parts, 1, arguments, 0, arguments.Length);
+
+        if (commands.ContainsKey(command)) {
+            commands[command](arguments);
+        } else {
+            InputText($"ERROR: Unknown command `{command}`");
+        }
+    }
+
+    private void RegisterCommand(string commandName, Action<string[]> action) {
+        commands.Add(commandName, action);
+    }
+
+// ----------------------------------------------------------------------------
+//     - Unity Stuff -
+// ----------------------------------------------------------------------------
     void Start() {
     }
 
     void Update() {
+        // unfocused + F1 = activate
+        // @FIXME: logic mistakes everywhere FIXIT !!
         if (consoleBox.text != "") {
             if (Input.GetKeyDown(KeyCode.Return)) {
-                ProcessCommand(consoleBox.text);
+                RunCommand(consoleBox.text);
                 consoleBox.text = "";
                 consoleBox.ActivateInputField();
+            } else if (Input.GetKeyDown(KeyCode.F1)) {
+                if (consoleBox.isFocused) {
+                    consoleBox.ActivateInputField();
+                }
             }
-        } else {
-            // @FIXME: if not focused and not empty and f1
-            if(!consoleBox.isFocused && Input.GetKeyDown(KeyCode.F1)) {
-                consoleBox.ActivateInputField();
-            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F1)) {
+            consoleBox.ActivateInputField();
         }
     }
 
     public void InputText(string text) {
-        if (commandHistory.Count >= maxHistory) {
-            Destroy(commandHistory[0].textObject.gameObject);
-            commandHistory.Remove(commandHistory[0]);
-        }
+        // @FIXME: find a better way to handle history
+        // if (commandHistory.Count >= maxHistory) {
+        //     Destroy(commandHistory[0].textObject.gameObject);
+        //     commandHistory.Remove(commandHistory[0]);
+        // }
 
         Command newCommand = new Command();
 
         newCommand.text = text;
-
         GameObject newText = Instantiate(textObject, consolePanel.transform);
 
         newCommand.textObject = newText.GetComponent<TMP_Text>();
-
         newCommand.textObject.text = newCommand.text;
 
         commandHistory.Add(newCommand);
     }
 
-    public void ProcessCommand(string text) {
-        string[] args = text.Split(' ');
-        string command = args[0];
+// ----------------------------------------------------------------------------
+//     - Commands -
+// ----------------------------------------------------------------------------
 
-        switch (command.ToLower()) {
-            case "clear":
-                ClearHistory();
-                break;
-            case "level":
-                if (args.Length > 1) {
-                    string levelName = args[1];
-                    InputText($"Loading level '{levelName}'");
-                } else {
-                    InputText("Please provide a level name");
-                }
-                break;
-            default:
-                InputText($"Unknown command: {args[0]}");
-                break;
-        }
-    }
-
-    public void ClearHistory() {
+    private void ClearCommand(string[] args) {
         foreach (Command command in commandHistory) {
             Destroy(command.textObject.gameObject);
         }
@@ -82,8 +104,26 @@ public class ConsoleManager : MonoBehaviour
         commandHistory.Clear();
     }
 
+    private void LevelCommand(string[] args) {
+        try {
+            string levelName = args[0];
+            InputText($"Loading level '{levelName}'");
+        } catch (Exception e) {
+            InputText($"ERROR: '{e}'");
+            InputText("ERROR: provide a level name");
+        }
+    }
+
+    private void HelpCommand(string[] args) {
+        InputText("Available Commands:");
+        InputText("- clear");
+        InputText("- level");
+    }
+
 }
 
+// @FIXME: has nothing to do with the console commands
+//         refactor it! 
 [System.Serializable]
 public class Command {
     public string text;
